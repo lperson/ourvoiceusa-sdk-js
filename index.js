4d79ae00d01fb339ad71280519be066ea72df19b
Larry Person <lp10011@gmail.com>
2019-01-19 21:27:47 -0500

    Added names of executive, upper and lower house

--- index.js
+++ index.js
@@ -1,64 +1,359 @@
 
 var us_states = {
-    "AL": "Alabama",
-    "AK": "Alaska",
-    "AS": "American Samoa",
-    "AZ": "Arizona",
-    "AR": "Arkansas",
-    "CA": "California",
-    "CO": "Colorado",
-    "CT": "Connecticut",
-    "DE": "Delaware",
-    "DC": "District Of Columbia",
-    "FM": "Federated States Of Micronesia",
-    "FL": "Florida",
-    "GA": "Georgia",
-    "GU": "Guam",
-    "HI": "Hawaii",
-    "ID": "Idaho",
-    "IL": "Illinois",
-    "IN": "Indiana",
-    "IA": "Iowa",
-    "KS": "Kansas",
-    "KY": "Kentucky",
-    "LA": "Louisiana",
-    "ME": "Maine",
-    "MH": "Marshall Islands",
-    "MD": "Maryland",
-    "MA": "Massachusetts",
-    "MI": "Michigan",
-    "MN": "Minnesota",
-    "MS": "Mississippi",
-    "MO": "Missouri",
-    "MT": "Montana",
-    "NE": "Nebraska",
-    "NV": "Nevada",
-    "NH": "New Hampshire",
-    "NJ": "New Jersey",
-    "NM": "New Mexico",
-    "NY": "New York",
-    "NC": "North Carolina",
-    "ND": "North Dakota",
-    "MP": "Northern Mariana Islands",
-    "OH": "Ohio",
-    "OK": "Oklahoma",
-    "OR": "Oregon",
-    "PW": "Palau",
-    "PA": "Pennsylvania",
-    "PR": "Puerto Rico",
-    "RI": "Rhode Island",
-    "SC": "South Carolina",
-    "SD": "South Dakota",
-    "TN": "Tennessee",
-    "TX": "Texas",
-    "UT": "Utah",
-    "VT": "Vermont",
-    "VI": "Virgin Islands",
-    "VA": "Virginia",
-    "WA": "Washington",
-    "WV": "West Virginia",
-    "WI": "Wisconsin",
-    "WY": "Wyoming"
+  "AL": {
+    name: "Alabama",
+    executive: "Governor",
+    upperHouse: "State Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "AK": {
+    name: "Alaska",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "AS": {
+    name: "American Samoa",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "AZ": {
+    name: "Arizona",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "AR": {
+    name: "Arkansas",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "CA": {
+    name: "California",
+    executive: "Governor",
+    upperHouse: "State Senate",
+    lowerHouse: "State Assembly"
+  },
+  "CO": {
+    name: "Colorado",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "CT": {
+    name: "Connecticut",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "DE": {
+    name: "Delaware",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "DC": {
+    name: "District Of Columbia",
+    executive: "Mayor",
+    upperHouse: "Council",
+    lowerHouse: ""
+  },
+  "FM": {
+    name: "Federated States Of Micronesia",
+    executive: "President", // also a vice president
+    upperHouse: "Senate",
+    lowerHouse: ""
+  },
+  "FL": {
+    name: "Florida",
+    executive: "Governor",
+    upperHouse: "State Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "GA": {
+    name: "Georgia",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "GU": {
+    name: "Guam",
+    executive: "Governor",
+    upperHouse: "Legislature",
+    lowerHouse: ""
+  },
+  "HI": {
+    name: "Hawaii",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "ID": {
+    name: "Idaho",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "IL": {
+    name: "Illinois",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "IN": {
+    name: "Indiana",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "IA": {
+    name: "Iowa",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "KS": {
+    name: "Kansas",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "KY": {
+    name: "Kentucky",
+    executive: "Governor",
+    upperHouse: "State Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "LA": {
+    name: "Louisiana",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "ME": {
+    name: "Maine",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "MH": {
+    name: "Marshall Islands",
+    executive: "President",
+    upperHouse: "Senate",
+    lowerHouse: ""
+  },
+  "MD": {
+    name: "Maryland",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Delegates"
+  },
+  "MA": {
+    name: "Massachusetts",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "MI": {
+    name: "Michigan",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "MN": {
+    name: "Minnesota",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "MS": {
+    name: "Mississippi",
+    executive: "Governor",
+    upperHouse: "State Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "MO": {
+    name: "Missouri",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "MT": {
+    name: "Montana",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "NE": {
+    name: "Nebraska",
+    executive: "Governor",
+    upperHouse: "Legislature",
+    lowerHouse: ""
+  },
+  "NV": {
+    name: "Nevada",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "Assembly"
+  },
+  "NH": {
+    name: "New Hampshire",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "NJ": {
+    name: "New Jersey",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "General Assembly"
+  },
+  "NM": {
+    name: "New Mexico",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "NY": {
+    name: "New York",
+    executive: "Governor",
+    upperHouse: "State Senate",
+    lowerHouse: "State Assembly"
+  },
+  "NC": {
+    name: "North Carolina",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "ND": {
+    name: "North Dakota",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "MP": {
+    name: "Northern Mariana Islands",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "OH": {
+    name: "Ohio",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "OK": {
+    name: "Oklahoma",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "OR": {
+    name: "Oregon",
+    executive: "Governor",
+    upperHouse: "State Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "PW": {
+    name: "Palau", // each state also elects a governor and has its own legislature
+    executive: 'President',
+    upperHouse: "Senate",
+    lowerHouse: "House of Delegates"
+  },
+  "PA": {
+    name: "Pennsylvania",
+    executive: "Governor",
+    upperHouse: "State Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "PR": {
+    name: "Puerto Rico",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "RI": {
+    name: "Rhode Island",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "SC": {
+    name: "South Carolina",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "SD": {
+    name: "South Dakota",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "TN": {
+    name: "Tennessee",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "TX": {
+    name: "Texas",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "UT": {
+    name: "Utah",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "VT": {
+    name: "Vermont",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "VI": {
+    name: "Virgin Islands",
+    executive: "Governor",
+    upperHouse: "Legislature",
+    lowerHouse: ""
+  },
+  "VA": {
+    name: "Virginia",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Delegates"
+  },
+  "WA": {
+    name: "Washington",
+    executive: "Governor",
+    upperHouse: "State Senate",
+    lowerHouse: "House of Represenatives"
+  },
+  "WV": {
+    name: "West Virginia",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Delegates"
+  },
+  "WI": {
+    name: "Wisconsin",
+    executive: "Governor",
+    upperHouse: "State Senate",
+    lowerHouse: "State Assembly"
+  },
+  "WY": {
+    name: "Wyoming",
+    executive: "Governor",
+    upperHouse: "Senate",
+    lowerHouse: "House of Represenatives"
+  },
 };
 
 var pip = require('point-in-polygon');
@@ -104,8 +399,8 @@ var geojson2polygons = function (json, flag) {
       polygons[0] = [];
       for (let g in polygon) {
         polygons[0].push({
-          [lng]: polygon[g][0],
-          [lat]: polygon[g][1],
+          [lng]: polygon[g][0], upperHouse: "Senate", lowerHouse: "House of Represenatives" },
+          [lat]: polygon[g][1], upperHouse: "Senate", lowerHouse: "House of Represenatives" },
         });
       }
     break;
@@ -115,8 +410,8 @@ var geojson2polygons = function (json, flag) {
       polygons[c] = [];
       for (let g in polygon) {
         polygons[c].push({
-          [lng]: polygon[g][0],
-          [lat]: polygon[g][1],
+          [lng]: polygon[g][0], upperHouse: "Senate", lowerHouse: "House of Represenatives" },
+          [lat]: polygon[g][1], upperHouse: "Senate", lowerHouse: "House of Represenatives" },
         });
       }
     }
