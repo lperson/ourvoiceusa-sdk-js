8315284c9d322ab11546fc68d24722d63222e933
Larry Person <lp10011@gmail.com>
2019-01-19 23:52:48 -0500

    fixed a syntax error

--- index.js
+++ index.js
@@ -4,355 +4,355 @@ var us_states = {
     name: "Alabama",
     exec: "Governor",
     sldu: "State Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "AK": {
     name: "Alaska",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "AS": {
     name: "American Samoa",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "AZ": {
     name: "Arizona",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "AR": {
     name: "Arkansas",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "CA": {
     name: "California",
     exec: "Governor",
     sldu: "State Senate",
-    sldl:: "State Assembly"
+    sldl: "State Assembly"
   },
   "CO": {
     name: "Colorado",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "CT": {
     name: "Connecticut",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "DE": {
     name: "Delaware",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "DC": {
     name: "District Of Columbia",
     exec: "Mayor",
     sldu: "Council",
-    sldl:: ""
+    sldl: ""
   },
   "FM": {
     name: "Federated States Of Micronesia",
     exec: "President", // also a vice president
     sldu: "Senate",
-    sldl:: ""
+    sldl: ""
   },
   "FL": {
     name: "Florida",
     exec: "Governor",
     sldu: "State Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "GA": {
     name: "Georgia",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "GU": {
     name: "Guam",
     exec: "Governor",
     sldu: "Legislature",
-    sldl:: ""
+    sldl: ""
   },
   "HI": {
     name: "Hawaii",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "ID": {
     name: "Idaho",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "IL": {
     name: "Illinois",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "IN": {
     name: "Indiana",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "IA": {
     name: "Iowa",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "KS": {
     name: "Kansas",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "KY": {
     name: "Kentucky",
     exec: "Governor",
     sldu: "State Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "LA": {
     name: "Louisiana",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "ME": {
     name: "Maine",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "MH": {
     name: "Marshall Islands",
     exec: "President",
     sldu: "Senate",
-    sldl:: ""
+    sldl: ""
   },
   "MD": {
     name: "Maryland",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Delegates"
+    sldl: "House of Delegates"
   },
   "MA": {
     name: "Massachusetts",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "MI": {
     name: "Michigan",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "MN": {
     name: "Minnesota",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "MS": {
     name: "Mississippi",
     exec: "Governor",
     sldu: "State Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "MO": {
     name: "Missouri",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "MT": {
     name: "Montana",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "NE": {
     name: "Nebraska",
     exec: "Governor",
     sldu: "Legislature",
-    sldl:: ""
+    sldl: ""
   },
   "NV": {
     name: "Nevada",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "Assembly"
+    sldl: "Assembly"
   },
   "NH": {
     name: "New Hampshire",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "NJ": {
     name: "New Jersey",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "General Assembly"
+    sldl: "General Assembly"
   },
   "NM": {
     name: "New Mexico",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "NY": {
     name: "New York",
     exec: "Governor",
     sldu: "State Senate",
-    sldl:: "State Assembly"
+    sldl: "State Assembly"
   },
   "NC": {
     name: "North Carolina",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "ND": {
     name: "North Dakota",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "MP": {
     name: "Northern Mariana Islands",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "OH": {
     name: "Ohio",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "OK": {
     name: "Oklahoma",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "OR": {
     name: "Oregon",
     exec: "Governor",
     sldu: "State Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "PW": {
     name: "Palau", // each state also elects a governor and has its own legislature
     exec: "President",
     sldu: "Senate",
-    sldl:: "House of Delegates"
+    sldl: "House of Delegates"
   },
   "PA": {
     name: "Pennsylvania",
     exec: "Governor",
     sldu: "State Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "PR": {
     name: "Puerto Rico",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "RI": {
     name: "Rhode Island",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "SC": {
     name: "South Carolina",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "SD": {
     name: "South Dakota",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "TN": {
     name: "Tennessee",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "TX": {
     name: "Texas",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "UT": {
     name: "Utah",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "VT": {
     name: "Vermont",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "VI": {
     name: "Virgin Islands",
     exec: "Governor",
     sldu: "Legislature",
-    sldl:: ""
+    sldl: ""
   },
   "VA": {
     name: "Virginia",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Delegates"
+    sldl: "House of Delegates"
   },
   "WA": {
     name: "Washington",
     exec: "Governor",
     sldu: "State Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
   "WV": {
     name: "West Virginia",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Delegates"
+    sldl: "House of Delegates"
   },
   "WI": {
     name: "Wisconsin",
     exec: "Governor",
     sldu: "State Senate",
-    sldl:: "State Assembly"
+    sldl: "State Assembly"
   },
   "WY": {
     name: "Wyoming",
     exec: "Governor",
     sldu: "Senate",
-    sldl:: "House of Represenatives"
+    sldl: "House of Represenatives"
   },
 };
 
